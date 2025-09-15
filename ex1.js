5f305c25b837648592f33872f5daa12515cc7a32
Nathan Le Biez <nthnlbz@gmail.com>
2025-09-15 17:38:20 +0200

    test de ce je connais pas en js

--- ex1.js
+++ ex1.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+console.log("Hello World");
+let string = " la bite "
+console.log(string); 
+
+const personnes = [
+    {prenom : "John", nom :"Doe", age : 25 , ville: "Rouen"},
+    {prenom : "Jane", nom :"Doe", age : 30 , ville: "Paris"},
+    {prenom : "Jim", nom :"Doe", age : 35 , ville: "Caen"}
+]
+console.table(personnes)
